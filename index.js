a046579a654975c06d158733c72fabfa3f1af5a8
NO078DJV\cat.nguyen <cat.nguyen@drjoy.vn>
2020-12-14 16:28:34 +0700

    updatev1

--- index.js
+++ index.js
@@ -6,11 +6,13 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-const googleIt = require("google-it");
 const serp = require("serp");
 const excel = require("node-excel-export");
 require("chromedriver");
-var webdriver = require('selenium-webdriver');
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+map = webdriver.promise.map;
 const { Driver } = require("selenium-webdriver/chrome");
 var promise = require('selenium-webdriver').promise;
 
@@ -19,9 +21,9 @@ var promise = require('selenium-webdriver').promise;
  */
 const app = express();
 const port = process.env.PORT || "8000";
-// var driver = new webdriver.Builder()
-//   .forBrowser('chrome')
-//   .build();
+var driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
 
 /**
  *  App Configuration
@@ -42,152 +44,120 @@ app.get("/", (req, res) => {
 })
 
 app.post("/", urlencodedParser, async (req, res) => {
-    //await driver.get('https://google.jp');
-    //await driver.findElement(webdriver.By.xpath("//*[@id='tsf']/div[2]/div[1]/div[1]/div/div[2]/input")).sendKeys("クリノリル錠50 日医工", webdriver.Key.ENTER);
-    // let linkText = await driver.findElements(webdriver.By.xpath("//*[@id='rso']/div/div/div[1]/a"));
-    // linkText.then(function (elements) {
-    //     var pendingHtml = elements.map(function (elem) {
-    //         return elem.getInnerHtml();
-    //     });
-    
-    //     promise.all(pendingHtml).then(function (allHtml) {
-    //         // `allHtml` will be an `Array` of strings
-    //         console.log(allHtml);
-    //     });
-    // });
-    // var pendingElements = driver.findElements(webdriver.By.xpath("//*[@id='rso']/div/div/div[1]/a"))
-
-    // pendingElements.then(function (elements) {
-    //     var pendingHtml = elements.map(function (elem) {
-    //         return elem.getInnerHtml();
-    //     });
-
-    //     promise.all(pendingHtml).then(function (allHtml) {
-    //         // `allHtml` will be an `Array` of strings
-            
-    // console.log(allHtml);
-    //     });
-    // });
-    // linkText.forEach(element => {
-    //     console.log(element.getText());
-    // })
-    
     let companyNameList = req.body.companyNameList.split("\n");
     let drugNameList = req.body.drugNameList.split("\n");
-    let getResultProcess = await (async () => {
-        let lastResult = [];
+
+    let resultList = await getData();
+    createExcel(resultList);
+
+    async function getData() {
+        let getResultProcess = [];
         for (let i = 0; i < companyNameList.length; i++) {
-
             let queryString = companyNameList[i] + drugNameList[i];
-         
+            await (await driver).sleep(2000);
+            await driver.get('https://google.jp');
+            await driver.findElement(By.xpath("//*[@id='tsf']/div[2]/div[1]/div[1]/div/div[2]/input")).sendKeys(queryString, webdriver.Key.ENTER);
+            let titleList = driver.findElements(By.xpath("//*[@id='rso']/div/div/div[1]/a/h3/span"));
+            let linkList = driver.findElements(By.xpath("//*[@id='rso']/div/div/div[1]/a"));
+            let titles = await map(titleList, e => e.getText())
+                .then(function (values) {
+                    return values;
+                });
 
-                // with request options
-                let result = await getSearchResult(queryString);
+            let title = titles.join("\n");
+            //console.log(titles);
 
-                let linkList = "";
+            let links = await map(linkList, e => e.getAttribute('href'))
+                .then(function (values) {
+                    return values;
+                });
+            let link = links.join("\n");
 
-                if (result.length > 0) {
-                    result.forEach(value => {
-                        linkList = linkList + value.url + "\n";
-                    });
-                }
-                lastResult.push({
-                    companyName: companyNameList[i],
-                    drugName: drugNameList[i],
-                    link: linkList
-                });;
+            getResultProcess.push({
+                "companyName": companyNameList[i],
+                "drugName": drugNameList[i],
+                "title": title,
+                "link": link,
+            })
+            //console.log(links);
         }
-        return lastResult;
-    })();
-
-    // You can define styles as json object
-    const styles = {
-        headerDark: {
-            fill: {
-                fgColor: {
-                    rgb: '00FE92'
-                }
-            },
-            font: {
-                color: {
-                    rgb: '000000'
-                },
-                sz: 12
-            }
-        },
-        linkStyle: {
-            alignment: {
-                wrapText: true,
-                vertical: "center"
-            }
-        }
-    };
-
-    //Here you specify the export structure
-    const specification = {
-        companyName: { // <- the key should match the actual data key
-            displayName: 'Company Name', // <- Here you specify the column header
-            headerStyle: styles.headerDark, // <- Header style
-            cellStyle: styles.linkStyle,
-            width: 220 // <- width in pixels
-        },
-        drugName: {
-            displayName: 'Drug Name',
-            headerStyle: styles.headerDark,
-            cellStyle: styles.linkStyle,
-            // cellFormat: function (value, row) { // <- Renderer function, you can access also any row.property
-            //     return (value == 1) ? 'Active' : 'Inactive';
-            // },
-            width: 200 // <- width in chars (when the number is passed as string)
-        },
-        link: {
-            displayName: 'Link',
-            headerStyle: styles.headerDark,
-            cellStyle: styles.linkStyle, // <- Cell style
-            width: 600 // <- width in pixels
-        }
+        return getResultProcess;
     }
 
-    // The data set should have the following shape (Array of Objects)
-    // The order of the keys is irrelevant, it is also irrelevant if the
-    // dataset contains more fields as the report is build based on the
-    // specification provided above. But you should have all the fields
-    // that are listed in the report specification
-    const dataset = [{
-            companyName: 'IBM',
-            drugName: 1,
-            link: 'some note'
-        },
-        {
-            companyName: 'IBM',
-            drugName: 1,
-            link: 'some note'
-        },
-        {
-            companyName: 'IBM',
-            drugName: 1,
-            link: 'some note'
-        }
-    ]
 
-
-    // Create the excel report.
-    // This function will return Buffer
-    const report = excel.buildExport(
-        [ // <- Notice that this is an array. Pass multiple sheets to create multi sheet report
-            {
-                name: 'Report', // <- Specify sheet name (optional)
-                specification: specification, // <- Report specification
-                data: getResultProcess // <-- Report data
+    function createExcel(resultList) {
+        // You can define styles as json object
+        const styles = {
+            headerDark: {
+                fill: {
+                    fgColor: {
+                        rgb: '00FE92'
+                    }
+                },
+                font: {
+                    color: {
+                        rgb: '000000'
+                    },
+                    sz: 12
+                }
+            },
+            linkStyle: {
+                alignment: {
+                    wrapText: true,
+                    vertical: "center"
+                }
             }
-        ]
-    );
+        };
 
-    // You can then return this straight
-    res.attachment('report.xlsx'); // This is sails.js specific (in general you need to set headers)
-    return res.send(report);
+        //Here you specify the export structure
+        const specification = {
+            companyName: { // <- the key should match the actual data key
+                displayName: 'Company Name', // <- Here you specify the column header
+                headerStyle: styles.headerDark, // <- Header style
+                cellStyle: styles.linkStyle,
+                width: 220 // <- width in pixels
+            },
+            drugName: {
+                displayName: 'Drug Name',
+                headerStyle: styles.headerDark,
+                cellStyle: styles.linkStyle,
+                // cellFormat: function (value, row) { // <- Renderer function, you can access also any row.property
+                //     return (value == 1) ? 'Active' : 'Inactive';
+                // },
+                width: 200 // <- width in chars (when the number is passed as string)
+            },
+            title: {
+                displayName: 'Title',
+                headerStyle: styles.headerDark,
+                cellStyle: styles.linkStyle, // <- Cell style
+                width: 600 // <- width in pixels
+            },
+            link: {
+                displayName: 'Link',
+                headerStyle: styles.headerDark,
+                cellStyle: styles.linkStyle, // <- Cell style
+                width: 600 // <- width in pixels
+            }
+        }
 
-    // OR you can save this buffer to the disk by creating a file.
+        // Create the excel report.
+        // This function will return Buffer
+        const report = excel.buildExport(
+            [ // <- Notice that this is an array. Pass multiple sheets to create multi sheet report
+                {
+                    name: 'Report', // <- Specify sheet name (optional)
+                    specification: specification, // <- Report specification
+                    data: resultList // <-- Report data
+                }
+            ]
+        );
+
+        // You can then return this straight
+        res.attachment('report.xlsx'); // This is sails.js specific (in general you need to set headers)
+        return res.send(report);
+
+        // OR you can save this buffer to the disk by creating a file.
+    }
 
 
 
